cf950bbc5014d9df0cb74508b140dcae4a4d08c1
Abdelrhman Amr <abdelrhmanamr639@gmail.com>
2025-09-30 08:25:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+
 #include "Image_Class.h"
 #include <iostream>
 using namespace std;
+bool validExtension(string filename) {
+    size_t dotPos = filename.find_last_of(".");
+    if (dotPos == -1) {
+        return false;
+    }
+    string ext = filename.substr(dotPos);
+    if (ext == ".jpg" || ext == ".bmp" || ext == ".png" || ext == ".tga") {
+        return true;
+    } else {
+        return false;
+    }
+}
 Image check_name(string& imgName) {
     Image img;
     while (true) {
@@ -30,6 +43,10 @@ void save_img(Image img, string& img_name, string imgName) {
         cout << "Please enter the new name with format: ";
         string new_name;
         cin >> new_name;
+        while (!validExtension(new_name)) {
+            cout << "Invalid extension! Please enter a valid one (.jpg, .bmp, .png, .tga): ";
+            cin >> new_name;
+        }
         img.saveImage(new_name);
         cout << "Image saved as " << new_name << endl;
     }
@@ -97,6 +114,127 @@ Image merge_img(Image img1, Image img2) {
         return merged_img;
     }
 }
+Image rotated_image(Image img )
+{
+    cout << "Please choose what degree adjustment do you want?" <<"\n"<< "90 degree - 180 degree - 270 degree" << endl;
+    int x;
+    while (true) {
+        cin >> x;
+
+        if (x == 90 || x == 180 || x == 270) {
+            Image final_img;
+            if (x == 90)
+            {
+                final_img = Image(img.height, img.width);
+                for (int i = 0; i < img.width; i++)
+                {
+                    for (int j = 0; j < img.height; j++)
+                    {
+                        int temp1 = img.height - 1 - j;
+                        int temp2 = i;
+                        for (int k = 0; k < 3; k++)
+                        {
+                            final_img(temp1, temp2, k) = img(i, j, k);
+                        }
+                    }
+                }
+                return final_img;
+            }
+            else if (x == 180)
+            {
+                final_img = Image(img.width, img.height);
+                for (int i = 0; i < img.width; i++)
+                {
+                    for (int j = 0; j < img.height; j++)
+                    {
+                        int temp1 = img.width - 1 - i;
+                        int temp2 = img.height - 1 - j;
+                        for (int k = 0; k < 3; k++)
+                        {
+                            final_img(temp1, temp2, k) = img(i, j, k);
+                        }
+                    }
+                }
+                return final_img;
+            }
+            else if (x == 270)
+            {
+                final_img = Image(img.height, img.width);
+                for (int i = 0; i < img.width; i++)
+                {
+                    for (int j = 0; j < img.height; j++)
+                    {
+                        int temp1 = j;
+                        int temp2 = img.width - 1 - i;
+                        for (int k = 0; k < 3; k++)
+                        {
+                            final_img(temp1, temp2, k) = img(i, j, k);
+                        }
+                    }
+                }
+                return final_img;
+            }
+        }
+        else {
+            cout << "Invalid input. Please enter 90, 180, or 270:" << endl;
+        }
+    }
+}
+Image BW(const Image& original) {
+
+    Image image = original;
+
+    for (int i = 0; i < image.width; ++i) {
+        for (int j = 0; j < image.height; ++j) {
+            unsigned int avg = 0;
+
+            for (int k = 0; k < 3; ++k) {
+                avg += image(i, j, k);
+            }
+
+            avg /= 3;
+
+            int val;
+            if (avg >= 128) {
+                val = 255;
+            }
+            else {
+                val = 0;
+            }
+
+            image(i, j, 0) = val;
+            image(i, j, 1) = val;
+            image(i, j, 2) = val;
+        }
+    }
+    return image;
+}
+Image cropImage(Image& image) {
+    int x,y,w,h;
+        cout << "Pls enter starting point: ";
+        cin >> x >> y;
+        cout << "please enter the dimensions of the area to cut: ";
+        cin >> w >> h;
+
+    if (x + w > image.width || y + h > image.height)
+    {
+        cout << "Error ";
+        exit(1);
+    }
+    else {
+        Image cropped(w, h);
+
+        for (int i = 0; i < w; ++i) {
+            for (int j = 0; j < h; ++j) {
+
+                for (int k = 0; k < 3; ++k) {
+                    cropped(i, j, k) = image(i + x, j + y, k);
+                }
+            }
+        }
+        return cropped;
+    }
+}
 int main() {
     string imgName;
     cout << "Pls enter image name to store new image\n";
@@ -109,7 +247,10 @@ int main() {
                 "2: Grey scale filter\n"
                 "3: Invert filter\n"
                 "4: Merge filter\n"
-                "5: Save the image\n"
+                "5:rotation filter\n"
+                "6:Black and While filter\n"
+                "7:Crop filter\n"
+                "8: Save the image\n"
                 "0: Exit\n";
         cin >> x;
         switch (x) {
@@ -133,7 +274,20 @@ int main() {
                 cout << "Images merged successfully." << endl;
                 break;
                 }
-            case 5:
+            case 5 : {
+                current_img = rotated_image(current_img);
+                break;
+            }
+            case 6 :
+            {
+                current_img = BW(current_img);
+                break;
+            }
+            case 7: {
+                current_img = cropImage(current_img);
+                break;
+            }
+            case 8:
                 save_img(current_img, imgName, imgName);
                 break;
             case 0: {
@@ -151,3 +305,10 @@ int main() {
     }
     return 0;
 }
+
+
+
+
+
+
+
